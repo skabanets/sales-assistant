980ea872b86a4b4e926db1d1f80f772da7daaec5
Sergey Kabanets <skabanets97@gmail.com>
2024-10-18 03:44:07 +0300

    Update Material UI themes

---
+++ src/mui-theme.d.ts
@@ -0,0 +1,55 @@
+import { PaletteOptions } from "@mui/material/styles/createPalette";
+
+declare module "@mui/material/styles/createPalette" {
+  interface Palette {
+    gray: {
+      100: string;
+      200: string;
+      300: string;
+      400: string;
+      600: string;
+      700: string;
+      800: string;
+    };
+    blue: {
+      A300: string;
+      100: string;
+      500: string;
+    };
+    score: {
+      0: string;
+      100: string;
+      150: string;
+      200: string;
+      250: string;
+    };
+    white: string;
+    black: string;
+  }
+
+  interface PaletteOptions {
+    gray?: {
+      100?: string;
+      200?: string;
+      300?: string;
+      400?: string;
+      600?: string;
+      700?: string;
+      800?: string;
+    };
+    blue?: {
+      A300?: string;
+      100?: string;
+      500?: string;
+    };
+    score?: {
+      0?: string;
+      100?: string;
+      150?: string;
+      200?: string;
+      250?: string;
+    };
+    white?: string;
+    black?: string;
+  }
+}
--- src/theme/darkTheme.ts
+++ src/theme/darkTheme.ts
@@ -1,4 +1,4 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from "@mui/material";
 
 export const darkTheme = createTheme({
   palette: {
@@ -9,12 +9,35 @@ export const darkTheme = createTheme({
     },
     text: {
       primary: "#FFFFFF",
-      secondary: "#252733",
+      secondary: "#EBECF0",
     },
     primary: {
       main: "#5B94FE",
     },
-    divider: "#414752",
+    gray: {
+      100: "#181A1F",
+      200: "#252733",
+      300: "#414752",
+      400: "#70737A",
+      600: "#B0B3B8",
+      700: "#C9CED6",
+      800: "#EBECF0",
+    },
+    blue: {
+      A300: "#3760AD",
+      100: "#181B29",
+      500: "#3E7FFF",
+    },
+    white: "#131314",
+    black: "#FFFFFF",
+    score: {
+      0: "#7A2C39",
+      100: "#6B3920",
+      150: "#705C0B",
+      200: "#2D662D",
+      250: "#295266",
+    },
+    divider: "#70737A",
   },
   typography: {
     fontFamily: "Popins, Roboto, sans-serif",
@@ -26,6 +49,7 @@ export const darkTheme = createTheme({
     MuiButton: {
       styleOverrides: {
         root: {
+          fontWeight: 500,
           backgroundColor: "#131314",
           color: "#EBECF0",
           border: "2px solid #3760AD",
