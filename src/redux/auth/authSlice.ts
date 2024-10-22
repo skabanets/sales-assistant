f2fcae5d97e5ef9456c8e78b905fbab008c2fb7a
Sergey Kabanets <skabanets97@gmail.com>
2024-10-23 00:48:07 +0300

    Add refresh thunk to handle token refreshing and user recovery

--- src/redux/auth/authSlice.ts
+++ src/redux/auth/authSlice.ts
@@ -1,6 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, isAnyOf } from "@reduxjs/toolkit";
+
 import { IAccountDTO } from "../../interfaces-submodule/interfaces/dto/account/iaccount.interface";
-import { login } from "./authOperations";
+import { login, recoverUser, refresh } from "./authOperations";
 
 interface IAuthSlice {
   userInfo: IAccountDTO | null;
@@ -23,10 +24,31 @@ const authSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: builder => {
-    builder.addCase(login.fulfilled, (state, { payload }) => {
-      state.userInfo = payload;
-      state.isLoggedIn = true;
-    });
+    builder
+      .addCase(refresh.pending, state => {
+        state.isRefreshing = true;
+        state.error = null;
+      })
+      .addCase(refresh.fulfilled, state => {
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refresh.rejected, () => {
+        return initialState;
+      })
+      .addMatcher(isAnyOf(login.pending, recoverUser.pending), state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addMatcher(isAnyOf(login.fulfilled, recoverUser.fulfilled), (state, { payload }) => {
+        state.userInfo = payload;
+        state.isLoggedIn = true;
+        state.isLoading = false;
+      })
+      .addMatcher(isAnyOf(login.rejected, recoverUser.rejected), (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload ?? "Login failed";
+      });
   },
   selectors: {
     selectUserInfo: state => state.userInfo,
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,2 +1,2 @@
 export * from "./auth/authSlice";
-export * from "./auth/authSlice";
+export * from "./auth/authOperations";
