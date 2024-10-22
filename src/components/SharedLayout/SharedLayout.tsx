7184c259c57055cdfe88a19d7aa9cb2ae8079077
Sergey Kabanets <skabanets97@gmail.com>
2024-10-22 20:43:37 +0300

    Update markup and styles for SharedLayout

--- src/components/SharedLayout/SharedLayout.tsx
+++ src/components/SharedLayout/SharedLayout.tsx
@@ -1,25 +1,32 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+import Box from "@mui/material/Box";
 
 import { Loader } from "../../components";
 
-import s from "./SharedLayout.module.css";
+import { asideStyles, headerStyles, layoutStyles, mainStyles } from "./SharedLayoutStyles";
+import { useAppSelector } from "../../hooks";
+import { selectIsLoggedIn } from "../../redux";
 
 export const SharedLayout = () => {
-  const isLoggedIn = true;
+  const isLoggedIn = useAppSelector(selectIsLoggedIn);
 
   return (
     <>
       {isLoggedIn ? (
-        <div className={s.layout}>
-          <header>Header</header>
-          <aside>Sidebar</aside>
-          <main>
+        <Box sx={layoutStyles}>
+          <Box component="header" sx={headerStyles}>
+            Header
+          </Box>
+          <Box component="aside" sx={asideStyles}>
+            Sidebar
+          </Box>
+          <Box component="main" sx={mainStyles}>
             <Suspense fallback={<Loader />}>
               <Outlet />
             </Suspense>
-          </main>
-        </div>
+          </Box>
+        </Box>
       ) : (
         <Suspense fallback={<Loader />}>
           <Outlet />
