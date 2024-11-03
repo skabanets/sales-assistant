99f5b623330036a735fbc697a200279f14e81078
Sergey Kabanets <skabanets97@gmail.com>
2024-11-03 20:10:13 +0200

    Add validation schema for editing chat name

---
+++ src/schemas/editChatFormSchema.ts
@@ -0,0 +1,9 @@
+import * as yup from "yup";
+
+export const editChatFormSchema = yup.object().shape({
+  name: yup
+    .string()
+    .required("This field is required")
+    .min(3, "Min name length: 3 characters")
+    .max(32, "Max name length: 32 characters"),
+});
