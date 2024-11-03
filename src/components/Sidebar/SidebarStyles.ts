e94c1ad2a36f5a2b7b877f2f141cfce2ea5c00a3
Sergey Kabanets <skabanets97@gmail.com>
2024-11-03 20:36:11 +0200

    Add Sidebar

---
+++ src/components/Sidebar/SidebarStyles.ts
@@ -0,0 +1,20 @@
+export const addChatBtnWrapper = {
+  padding: "12px 16px 0 16px",
+};
+
+export const chatMenuStyles = {
+  display: "flex",
+  flexDirection: "column",
+  height: "calc(100% - 120px)",
+};
+
+export const mainMenuStyles = {
+  padding: "12px 16px",
+  height: "120px",
+  borderTop: "1px solid",
+  borderColor: "gray.200",
+};
+
+export const feedsNavLinkStyles = {
+  justifyContent: "start",
+};
