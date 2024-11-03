407553d65f8337f43f61dc0f5d63fb026dc5cf6c
Sergey Kabanets <skabanets97@gmail.com>
2024-11-03 20:31:29 +0200

    Add Center Screen modal

---
+++ src/components/CenterScreenModal/CenterScreenModalStyles.ts
@@ -0,0 +1,34 @@
+export const wrapperModalStyles = {
+  padding: "32px",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "560px",
+  bgcolor: "background.paper",
+  boxShadow: "0px 2px 8px 0px rgba(0, 0, 0, 0.08), 0px 16px 40px 0px rgba(0, 0, 0, 0.16)",
+  display: "flex",
+  flexDirection: "column",
+  gap: "27px",
+  borderRadius: "24px",
+};
+
+export const titleWrapperStyles = {
+  display: "flex",
+  alignItems: "center",
+};
+
+export const modalTitleStyles = {
+  fontSize: "24px",
+  fontStyle: "normal",
+  fontWeight: "500",
+  lineHeight: "120%",
+  width: "100%",
+};
+
+export const closeButtonStyles = {
+  width: "32px !important",
+  height: "32px",
+  padding: "4px",
+  flexShrink: 0,
+};
