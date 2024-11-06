4f588e2b85fe1e9dff225e8a374f2c70b7f3c17b
Sergey Kabanets <skabanets97@gmail.com>
2024-11-06 03:49:34 +0200

    Add re-export files for components and hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,6 @@
 export * from "./reduxHooks";
 export * from "./useLoginForm";
+export * from "./useModal";
+export * from "./useEscapeKeyClose";
+export * from "./useEditChatForm";
+export * from "./useMenu";
