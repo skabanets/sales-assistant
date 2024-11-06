95cff3c7fce05a76b21b4b3ce504dcf53b3c243a
Sergey Kabanets <skabanets97@gmail.com>
2024-11-06 03:52:22 +0200

    Update layout structure and add styles for smooth sidebar toggle

--- src/components/SharedLayout/SharedLayout.tsx
+++ src/components/SharedLayout/SharedLayout.tsx
@@ -1,27 +1,44 @@
-import { Suspense } from "react";
+import { Suspense, useContext } from "react";
 import { Outlet } from "react-router-dom";
 import Box from "@mui/material/Box";
 
-import { Loader } from "../../components";
+import { Header, Loader, Sidebar } from "../../components";
 
-import { asideStyles, headerStyles, layoutStyles, mainStyles } from "./SharedLayoutStyles";
 import { useAppSelector } from "../../hooks";
 import { selectIsLoggedIn } from "../../redux";
+import { ThemeContext } from "../../context/ThemeContext";
+import {
+  closedSidebarStyles,
+  layoutStylesWithSidebar,
+  layoutStylesWithoutSidebar,
+  mainStylesWithSidebar,
+  mainStylesWithoutSidebar,
+  openSidebarStyles,
+} from "./SharedLayoutStyles";
 
 export const SharedLayout = () => {
   const isLoggedIn = useAppSelector(selectIsLoggedIn);
 
+  const themeContext = useContext(ThemeContext);
+
+  if (!themeContext) {
+    throw new Error("ThemeSwitcher must be used within a ThemeContextProvider");
+  }
+
+  const { isSidebarOpen } = themeContext;
+
   return (
     <>
       {isLoggedIn ? (
-        <Box sx={layoutStyles}>
-          <Box component="header" sx={headerStyles}>
-            Header
+        <Box sx={isSidebarOpen ? layoutStylesWithSidebar : layoutStylesWithoutSidebar}>
+          <Header />
+          <Box component="aside" sx={isSidebarOpen ? openSidebarStyles : closedSidebarStyles}>
+            <Sidebar />
           </Box>
-          <Box component="aside" sx={asideStyles}>
-            Sidebar
-          </Box>
-          <Box component="main" sx={mainStyles}>
+          <Box
+            component="main"
+            sx={isSidebarOpen ? mainStylesWithSidebar : mainStylesWithoutSidebar}
+          >
             <Suspense fallback={<Loader />}>
               <Outlet />
             </Suspense>
--- src/components/SharedLayout/SharedLayoutStyles.ts
+++ src/components/SharedLayout/SharedLayoutStyles.ts
@@ -1,18 +1,65 @@
-export const layoutStyles = {
+export const layoutStylesWithSidebar = {
   display: "grid",
   width: "100vw",
   height: "100vh",
   gridTemplateAreas: "'aside head' 'aside main'",
   gridTemplateColumns: "320px 1fr",
   gridTemplateRows: "72px 1fr",
+  overflow: "hidden",
+  transition: "grid-template-columns 0.8s ease",
 };
 
-export const headerStyles = { gridArea: "head" };
+export const layoutStylesWithoutSidebar = {
+  display: "grid",
+  width: "100vw",
+  height: "100vh",
+  gridTemplateAreas: "'head head' 'main main'",
+  gridTemplateColumns: "1fr",
+  gridTemplateRows: "72px 1fr",
+  transition: "grid-template-columns 0.8s ease",
+};
 
-export const asideStyles = {
+const sidebarCommonStyles = {
   gridArea: "aside",
-  width: "320px",
   backgroundColor: "background.paper",
+  position: "absolute",
+  height: "100%",
+  transition: "transform 0.8s ease, opacity 0.8s ease",
+  pointerEvents: "none",
+  zIndex: -1,
 };
 
-export const mainStyles = { gridArea: "main" };
+export const openSidebarStyles = {
+  ...sidebarCommonStyles,
+  width: "320px",
+  transform: "translateX(0)",
+  opacity: 1,
+  zIndex: 1,
+};
+
+export const closedSidebarStyles = {
+  ...sidebarCommonStyles,
+  width: "320px",
+  transform: "translateX(-100%)",
+  opacity: 0,
+};
+
+const mainCommonStyles = {
+  gridArea: "main",
+  padding: "0 32px",
+  height: "calc(100vh - 72px)",
+  transition: "transform 0.8s ease, width 0.8s ease",
+  zIndex: 1,
+};
+
+export const mainStylesWithSidebar = {
+  ...mainCommonStyles,
+  transform: "translateX(320px)",
+  width: "calc(100vw - 320px)",
+};
+
+export const mainStylesWithoutSidebar = {
+  ...mainCommonStyles,
+  transform: "translateX(0)",
+  width: "100vw",
+};
