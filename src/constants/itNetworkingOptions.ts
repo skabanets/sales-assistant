cadb70bf8b726022c8e0c940bae79c4a445b3f54
Sergey Kabanets <skabanets97@gmail.com>
2024-11-09 17:36:16 +0200

    Add constants itemsPerPageOptions and itNetworkingOptions

---
+++ src/constants/itNetworkingOptions.ts
@@ -0,0 +1,5 @@
+export const itNetworkingOptions = [
+  { value: "it_networking_1", label: "IT Networking 1" },
+  { value: "it_networking_2", label: "IT Networking 2" },
+  { value: "it_networking_3", label: "IT Networking 3" },
+];
