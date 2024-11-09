26633f9155659dc4c7325fd5bd07ce7f1d6956a0
Sergey Kabanets <skabanets97@gmail.com>
2024-11-09 17:38:01 +0200

    Add FeedControlPanel component

---
+++ src/components/FeedControlPanel/FeedControlPanelStyles.ts
@@ -0,0 +1,17 @@
+export const controlPanelStyles = {
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "start",
+  gap: "8px",
+};
+
+export const itNetworkSelectStyles = {
+  height: "48px",
+  width: "600px",
+};
+
+export const refreshBtnStyles = {
+  maxWidth: "161px",
+  backgroundColor: "background.paper",
+};
