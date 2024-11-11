c1d5d6d2bc5e01ca7b9b6ea93957eb5cee639605
Sergey Kabanets <skabanets97@gmail.com>
2024-11-11 09:26:18 +0200

    Add sort label and title input field for upwork feed table

---
+++ src/components/UpworkFeedTable/TitleInput/TitleInput.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import TextField from "@mui/material/TextField";
+
+import { CustomIcon } from "../../../components";
+
+import { useUniversalSearchParams } from "../../../hooks";
+import { tableInputStyles } from "../UpworkFeedTableStyles";
+
+export const TitleFilterInput = () => {
+  const { searchParams, setParam } = useUniversalSearchParams();
+  const initialTitle = searchParams.get("title") || "";
+  const [value, setValue] = useState(initialTitle);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const title = e.target.value;
+    setValue(title);
+    setParam("title", title, 1000);
+  };
+
+  const handleClear = () => {
+    setValue("");
+    setParam("title", "");
+  };
+
+  return (
+    <TextField
+      type="text"
+      label=""
+      variant="filled"
+      autoComplete="off"
+      fullWidth
+      sx={tableInputStyles}
+      value={value}
+      onChange={handleChange}
+      slotProps={{
+        input: {
+          endAdornment: (
+            <InputAdornment position="end">
+              {value && (
+                <IconButton
+                  onClick={handleClear}
+                  edge="end"
+                  sx={{ width: "28px", height: "28px", padding: 0 }}
+                >
+                  <CustomIcon iconName="x-circle" htmlColor="gray.600" />
+                </IconButton>
+              )}
+            </InputAdornment>
+          ),
+        },
+      }}
+    />
+  );
+};
