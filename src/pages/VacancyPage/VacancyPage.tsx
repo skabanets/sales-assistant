29b32e5480ffa48eee66fddd50d87a361c2f3981
Sergey Kabanets <skabanets97@gmail.com>
2024-11-14 09:39:56 +0200

    Add vacancy title component

--- src/pages/VacancyPage/VacancyPage.tsx
+++ src/pages/VacancyPage/VacancyPage.tsx
@@ -1,5 +1,22 @@
+import { useParams } from "react-router-dom";
+import { Loader, VacancyTitle } from "../../components";
+import { useGetFeedByIdQuery } from "../../services";
+
 const VacancyPage = () => {
-  return <div>VacancyPage</div>;
+  const { id } = useParams<{ id: string }>();
+
+  const feedId = id ?? "";
+
+  const { data, isLoading } = useGetFeedByIdQuery(feedId);
+
+  if (isLoading) return <Loader />;
+
+  return (
+    <div>
+      <VacancyTitle title={data?.title} />
+      VacancyPage
+    </div>
+  );
 };
 
 export default VacancyPage;
