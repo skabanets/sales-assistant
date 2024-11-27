108a4cd567e221c4ebb1ec6a5c0166f1a6d08e1f
Sergey Kabanets <skabanets97@gmail.com>
2024-11-27 11:23:18 +0200

    Update ChatMessage component

--- src/components/ChatMessage/ChatMessage.tsx
+++ src/components/ChatMessage/ChatMessage.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
@@ -14,18 +14,27 @@ import {
 import { IMessageDTO } from "../../interfaces-submodule/interfaces/dto/message/imessage-dto";
 
 interface IChatMessageProps {
+  text?: string;
   message?: IMessageDTO;
 }
-export const ChatMessage: FC<IChatMessageProps> = ({ message }) => {
+export const ChatMessage: FC<IChatMessageProps> = ({
+  text = "Hello, how can I assist you today?",
+  message,
+}) => {
+  const [messageText, setMessageText] = useState<string>("");
+
+  useEffect(() => {
+    setMessageText(message ? message.content : text);
+  }, [message]);
+
   const icon = message && !message.isBot ? "user" : "chat";
-  const text = message ? message.content : "Hello, how can I assist you today?";
 
   return (
     <Box sx={messageWrapperStyles(message?.isBot)}>
       <Box sx={chatIconWrapperStyles}>
         <CustomIcon iconName={icon} sx={chatIconStyles(message?.isBot)} />
       </Box>
-      <Typography sx={textMessageStyles}>{text}</Typography>
+      <Typography sx={textMessageStyles}>{messageText}</Typography>
     </Box>
   );
 };
