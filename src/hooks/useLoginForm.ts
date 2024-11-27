bd9676d587539db7ad1e636f8981366de65e841c
Sergey Kabanets <skabanets97@gmail.com>
2024-11-27 12:43:21 +0200

    Add error display for user authentication

--- src/hooks/useLoginForm.ts
+++ src/hooks/useLoginForm.ts
@@ -7,9 +7,11 @@ import { useLoginMutation } from "../services";
 import { setUserInfo } from "../redux";
 import { loginFormSchema } from "../schemas/loginFormSchema";
 import { ILoginRequestDTO } from "../interfaces-submodule/interfaces/dto/auth/iadmin-login-request.interface";
+import { getFormatErrorMessage } from "../heplers";
 
 export const useLoginForm = () => {
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
   const dispatch = useAppDispatch();
   const [login] = useLoginMutation();
 
@@ -27,7 +29,19 @@ export const useLoginForm = () => {
 
   const onSubmit = async (credentials: ILoginRequestDTO) => {
     try {
-      const data = await login(credentials).unwrap();
+      const data = await login(credentials)
+        .unwrap()
+        .catch(error => {
+          const errorCode = error?.data?.error?.errorCode;
+          if (typeof errorCode === "string") {
+            const errorMessage = getFormatErrorMessage(errorCode);
+            setError(errorMessage || "Unknown error");
+          } else {
+            setError("An unexpected error occurred.");
+          }
+          throw error;
+        });
+
       dispatch(setUserInfo(data));
       reset();
     } catch (error) {
@@ -42,5 +56,6 @@ export const useLoginForm = () => {
     handleSubmit,
     onSubmit,
     errors,
+    error,
   };
 };
