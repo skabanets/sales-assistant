655af8575c19511c1bc21f98a932a64ee558b4c0
Sergey Kabanets <skabanets97@gmail.com>
2024-12-19 01:36:06 +0200

    Delete scoreOptions

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,6 @@ export * from "./theme";
 export * from "./itemsPerPageOptions";
 export * from "./itNetworkingOptions";
 export * from "./keywordOptions";
-export * from "./scoreOptions";
 export * from "./matchedItemType";
 export * from "./casesKeys";
 export * from "./blogsKeys";
