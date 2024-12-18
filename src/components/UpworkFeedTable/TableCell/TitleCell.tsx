9d8b9734a7367c0a1451fcd946798d46d95c7e91
Sergey Kabanets <skabanets97@gmail.com>
2024-12-19 01:37:08 +0200

    Update TitleCell

--- src/components/UpworkFeedTable/TableCell/TitleCell.tsx
+++ src/components/UpworkFeedTable/TableCell/TitleCell.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { useLocation, useNavigate } from "react-router";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 
@@ -6,13 +7,20 @@ import { linkStyles, linkWrapperStyles } from "../UpworkFeedTableStyles";
 
 interface ITitleCellProps {
   title: string;
-  url: string;
+  id: string;
 }
 
-export const TitleCell: FC<ITitleCellProps> = ({ title, url }) => {
+export const TitleCell: FC<ITitleCellProps> = ({ title, id }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleClickTitle = () => {
+    navigate(`/upwork-feeds/${id}`, { state: { from: location } });
+  };
+
   return (
     <Box sx={linkWrapperStyles}>
-      <Link href={url} target="_blank" rel="noopener noreferrer" sx={linkStyles}>
+      <Link onClick={handleClickTitle} target="_blank" rel="noopener noreferrer" sx={linkStyles}>
         {title}
       </Link>
     </Box>
